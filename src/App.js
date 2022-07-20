535dca695334cdd97c2ade2112bb0d088c3ee2cf
Nandhu1296 <nandhu1296@ymail.com>
2022-07-19 19:24:38 -0500

    git commit after footer change

--- src/App.js
+++ src/App.js
@@ -3,12 +3,15 @@ import React from 'react'
 
 import Navbar from './Navbar';
 
+import Footer from './Footer';
+
 const App=()=>
 {
     return(
     <div>
         <h1>nandha kumar</h1>
         <Navbar/>
+        <Footer></Footer>
         </div>
     );
 }
