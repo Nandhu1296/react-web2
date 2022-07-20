63520211d3d31ebde2671ac0c5984c228cfb91d0
Nandhu1296 <nandhu1296@ymail.com>
2022-07-19 19:11:17 -0500

    2nd commit after nav bar implementation

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+
+import Navbar from './Navbar';
+
+const App=()=>
+{
+    return(
+    <div>
+        <h1>nandha kumar</h1>
+        <Navbar/>
+        </div>
+    );
+}
+
+export default App;
--- src/Index.js
+++ src/Index.js
@@ -1,1 +1,7 @@
-xvsg
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+import App from "./App.js"
+
+
+ReactDOM.render(<App/>, document.getElementById("root"))
